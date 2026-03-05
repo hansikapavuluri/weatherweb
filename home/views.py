e30e0125cb479bf195a0aee21c9131dfdee1130d
hansikapavuluri <hansikapavuluri2007@gmail.com>
2026-03-05 23:11:43 +0530

    Add .gitignore and remove unnecessary files

--- home/views.py
+++ home/views.py
@@ -8,14 +8,14 @@ def home(request):
     if request.method == "POST":
         city=request.POST.get('city')
         if not city:
-            messages.error(request,"Invalid city")
+            messages.error(request,"City is invalid")
             return render(request,'index.html')
         try:
             api_key='c84c3ce39e006519dd4de2a9129a8e04'
             url=f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
             response=requests.get(url,timeout=5)
             if response.status_code != 200:
-                messages.error(request, "City not found or API error")
+                messages.error(request, "City not found")
                 return render(request, 'index.html')
             data=response.json()
             weather_data={
@@ -26,7 +26,7 @@ def home(request):
             }
             return render(request,'index.html',{'weather':weather_data,'city':city})
         except:
-            messages.error(request,"City is invalid")
+            messages.error(request,"City is invalid or API Error")
             return render(request,'index.html')
     else:
         return render(request,'index.html') 
